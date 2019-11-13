f8d95f4df3ca409213da9862400f83bec50b3868
Dalia <dalia.alotaibi@hotmail.com>
2019-11-13 13:14:58 +0300

    send_hw_week6
    it is easy :)

---
+++ DaliaAlotaibi/week6/js/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function()
+{
+$("img").hide();
+$("#showimage").click(function()
+    {
+        $("img").show();
+    }
+);
+$("#hideimage").click(function()
+    {
+        $("img").hide();
+    }
+);
+});
