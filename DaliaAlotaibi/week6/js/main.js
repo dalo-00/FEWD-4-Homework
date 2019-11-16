919cf6aeba2e3d1c28192406ba85a632f7bd5c77
Dalia <dalia.alotaibi@hotmail.com>
2019-11-16 18:35:18 +0300

    hw6 after update
    it is good :)

--- DaliaAlotaibi/week6/js/main.js
+++ DaliaAlotaibi/week6/js/main.js
@@ -1,11 +1,18 @@
 
 $(document).ready(function()
 {
+
+var pets=["beagle.jpg","bunny.jpg","cat.jpg","iguana.jpg","pony.jpg"];
+
 $("img").hide();
 $("#showimage").click(function()
     {
-        $("img").show();
-    }
+        for(var i=0;i<pets.length;i++)
+        { 
+            $(".listpets").append("<li><img src=img/" + pets[i] + "></li>");
+        }     
+     }       
+       
 );
 $("#hideimage").click(function()
     {
